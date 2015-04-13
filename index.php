645a823fb476acea422f452f8761d22bce30f279
Grace Cabana <gmcabanah@unal.edu.co>
2015-04-12 22:34:35 -0500

    Código fuente de la versión 1.0 que incluye cambios basados en revisiones y comentarios hasta el correo enviado el 11 de abril.

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Filatelia</title>
+</head>
+
+<style>
+h3 {
+    font-family: Verdana, Geneva, sans-serif;
+	font-size: 14px;
+	color: #FFF;
+	background-color: #333;
+	text-align: center;
+	margin: 0px;
+	padding: 12px;
+	background-image: url(imagenes/fondo_pie.jpg);
+	background-repeat: repeat-x;
+}
+h1{
+	font-family: Verdana, Geneva, sans-serif;
+	margin-left: 10px;
+	text-align: center;
+	font-size: 18px;
+	line-height: 1
+}
+h2{
+	font-family: Verdana, Geneva, sans-serif;
+	margin-left: 10px;
+	margin-bottom: 0px;
+	color:#003;
+	font-size: 20px;
+	text-align: center;
+}
+h4 {
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 14px;
+	font-weight: bold;
+	color: #36F;
+	text-align: right;
+	margin: 0px;
+	padding-top: 12px;
+	vertical-align: text-bottom
+}
+h5{
+	font-size: 16px;
+	padding:10px; 
+	padding-bottom:0px;
+	font-family:Verdana, Geneva, sans-serif; 
+	line-height:1.5; 
+	text-align:justify
+}
+p{
+	font-size: 14px;
+	padding:10px; 
+	font-family:Verdana, Geneva, sans-serif; 
+	line-height:1.5; 
+	text-align:justify
+}
+/*.book
+{
+	border-bottom: #000 thin solid; 
+	border-top: #000 thin solid;	
+	vertical-align: central
+}*/
+</style>
+
+<body background="Images/bg_grey.jpg">
+
+<body>
+<table width="960" border="0" align="center" cellpadding="0" cellspacing="0" style="background:#E2DEDF;">
+  <tr>
+    <td><img src="Images/logotipo.jpg" width="960" height="110" alt="logo" /></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('home','','Images/btn_home02.jpg',1)"><img src="Images/btn_home02.jpg" alt="home" name="home" width="192" height="45" border="0" id="home" /></a></td>
+        
+        <td><a href="contact.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('contact','','Images/btn_contact02.jpg',1)"><img src="Images/btn_contact01.jpg" alt="contact" name="contact" width="192" height="45" border="0" id="contact" /></a></td>
+        <td><img src="Images/btn_vacio.png" alt="clientes" name="clientes" width="192" height="45" border="0" id="clientes" /></a></td>
+        <td><img src="Images/btn_vacio.png" alt="contacto" name="contacto" width="192" height="45" border="0" id="contacto" /></td>
+        <td><img src="Images/btn_vacio.png" alt="contacto" name="contacto" width="192" height="45" border="0" id="contacto" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><img src="Images/banner.jpg" width="960" height="270" alt="banner" /></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellpadding="0" cellspacing="0" class="blanco">
+      <tr>
+      <td>
+      </td>
+      	<p style="text-align:center; font-size:18px">
+          I am a Geek lover of philately. In here you can take a look at my personal collection, if you have a comment that you want to share or and offer please contact me by email.       </p><h1>Feel free to make and offer and contact me :)</h1>
+          <h2><br/>Transfer conditions</h2> <p style="text-align:center">Payment by Personal In the Netherlands, I live in Rotterdam also Could be  or Bankers Transfer In the Netherlands. Shipping fee must be paid by buyer. Stamps are not sold individually, you may buy the entire book if interested. </p>
+     
+<h2><br/>Return Policy</h2> <p style="text-align:center">No return.</p>
+
+<h2><br/>Books</h2>
+
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellpadding="10" cellspacing="0" class="blanco" align="center">
+      <tr>
+      <td class="book">      
+      <h5><strong>Book 1</strong></h5>
+      <p style="vertical-align: central">
+<strong>Country:</strong> Switzerland<br/>
+<strong><strong>Pages:</strong></strong> 9<br/>
+<strong>Number of Stamps aprox:</strong> 555 <br/>
+<strong>Description: </strong><br/>
+Great collection of stamps!!!
+More than 500 stamps of the  country of Switzerland. Only one chance. If you are not a professional collector this is the best opportunity to start and if you are already, there are a variety of stamps that will allow you to complete your collection! Selling entire collection, not stamps per unit.
+      <a href="Switzerland.html">
+      <h4>See more...</h4></a></td>
+      <td class="book" align="center">
+      <img src="Images/provisional1.jpg" alt="Switzerland" align="left" />
+      </td>
+      </tr>
+      <tr>
+      <td>
+      <h5><strong>Book 2</strong></h5><p>
+<strong>Country:</strong> Netherlands<br/>
+<strong><strong>Pages:</strong></strong> 12<br/>
+<strong>Number of Stamps aprox:</strong> 749<br/>
+<strong>Description: </strong><br/>
+Great collection of stamps!!! More than 700 stamps of the country of Netherlands. Only one chance. If you are not a professional collector this is the best opportunity to start and if you are already, there are a variety of stamps that will allow you to complete your collection! Selling entire collection, not stamps per unit. 
+
+<a href="Netherlands.html">
+<h4>See more...</h4></a></td>
+      <td align="center">
+      <img src="Images/provisional2.jpg" alt="Netherlands" align="left" />
+      </td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+  <td>
+      <table width="100%" border="0" cellpadding="10" cellspacing="0" class="blanco" align="center">
+      <tr>
+      <td>
+      <h5><strong>Book 3 (T1)</strong></h5>
+      <p style="text-align:left">
+<strong>Country:</strong> Several
+(PARAGUAY - MEXICO - ECUADOR - PANAMA - CHILE - ARGENTINA 
+- BRAZIL - EGYPT - CUBA - BOLIVIA - URUGUAY – COLOMBIA)
+<br/>
+<strong>Pages:</strong> 12<br/>
+<strong>Number of Stamps aprox:</strong> 432</p>
+      </td>
+      <td align="center">
+      <img src="Images/provisional2.jpg" alt="Several" />
+      </td></tr>
+      <tr>
+      <td>
+      <h5><strong>Book 4 (T2)</strong></h5>
+      <p style="text-align:left">
+<strong>Country:</strong> Several
+(SAN MARINO REPUBLIC - AUSTRALIA - NEW ZEALAND - NETHERLANDS - CHINA - HONDURAS - HAITI - COSTA RICA - IRAQ - TURKEY - GUATEMALA - PHILIPPINES - ISRAEL - PORTUGAL)<br/>
+<strong>Pages:</strong> 12<br/>
+<strong>Number of Stamps aprox:</strong> 432</p>
+      </td>
+      <td align="center">
+      <img src="Images/provisional2.jpg" alt="Several2" />
+      </td>
+      </tr>
+      <tr>
+      <td>
+      <h5><strong>Book 5 (T3)</strong></h5>
+      <p style="text-align:left">
+<strong>Country:</strong> Several
+(YUGOSLAVIA - RUSSIA - MADAGASCAR - MALASIA - FINLAND - HELVETIA - GERMANY - POLISHCA - BELGIUM – SPAIN)<br/>
+<strong>Pages:</strong> 12<br/>
+<strong>Number of Stamps aprox:</strong> 432</p>
+      </td>
+      <td align="center">
+      <img src="Images/provisional2.jpg" alt="Several2" />
+      </td>
+      </tr>
+      </table>
+  </td></tr>
+  <tr>
+    <td><table width="100%" border="0" cellpadding="10" cellspacing="0" class="blanco" align="center">
+      <tr>
+      <td>
+      <h5><strong>Book 6 (T4)</strong><br/></h5>
+      <p>
+        <strong>Country:</strong> Italy<br/>
+<strong>Pages:</strong> 12<br/>
+<strong>Number of Stamps aprox:</strong> 432</p>
+      </td>
+      <td align="center">
+      <img src="Images/provisional1.jpg" alt="Italy" align="left" />
+      </td>
+      </tr>
+      <tr>
+      <td>
+      <h5><strong>Book 7 (T5)</strong></h5>
+      <p>
+<strong>Country:</strong> Several<br/>
+<strong>Pages:</strong> 30<br/>
+<strong>Number of Stamps aprox:</strong> 1080</p>
+      </td>
+      <td align="center">
+      <img src="Images/provisional2.jpg" alt="Several3" align="left" />
+      </td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><h3>e-mail: sellingmystampcollection@gmail.com<br/>
+    Copyright  Fegui and Grace - 2015</h3>
+    <?PHP include ("counter.php"); ?>
+    </td>
+  </tr>
+</table>
+</body>
+
+</body>
+</html>
